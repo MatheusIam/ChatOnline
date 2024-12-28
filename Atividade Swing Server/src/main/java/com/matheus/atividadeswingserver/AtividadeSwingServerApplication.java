204ffc692c73067f44999b3c0a12ad9670ca0f1c
Matheus Iam <matheusiam27@gmail.com>
2024-12-28 15:41:45 -0400

    first commit

---
+++ Atividade Swing Server/src/main/java/com/matheus/atividadeswingserver/AtividadeSwingServerApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.atividadeswingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeSwingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtividadeSwingServerApplication.class, args);
+    }
+
+}
---
+++ Atividade Swing Server/src/test/java/com/matheus/atividadeswingserver/AtividadeSwingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.atividadeswingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeSwingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
